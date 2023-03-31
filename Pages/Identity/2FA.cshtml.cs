c89c68bba6c0cb4ca56cedc46c22d183237aaf6b
IKECHUKWU <sundayjabikem@gmail.com>
2023-03-31 21:24:01 +0100

    update

---
+++ Pages/Identity/2FA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book_Review_UI.Pages.Identity
+{
+    public class _2FAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
